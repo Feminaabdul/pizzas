0af1aa32c04c3248b05f39dbb75c336066cdc1f4
Feminaabdul <zain.3email.com@gmail.com>
2024-01-25 18:11:36 +0530

    home page

---
+++ public/js/app.js
@@ -0,0 +1,35 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./resources/js/app.js":
+/*!*****************************!*\
+  !*** ./resources/js/app.js ***!
+  \*****************************/
+/***/ (() => {
+
+console.log('appjs');
+
+/***/ }),
+
+/***/ "./resources/scss/app.scss":
+/*!*********************************!*\
+  !*** ./resources/scss/app.scss ***!
+  \*********************************/
+/***/ (() => {
+
+throw new Error("Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nSassError: Undefined variable.\n   ╷\n39 │         background: $d;\r\n   │                     ^^\n   ╵\n  resources\\scss\\app.scss 39:21  root stylesheet\n    at processResult (F:\\realtimepizza\\node_modules\\webpack\\lib\\NormalModule.js:764:19)\n    at F:\\realtimepizza\\node_modules\\webpack\\lib\\NormalModule.js:866:5\n    at F:\\realtimepizza\\node_modules\\loader-runner\\lib\\LoaderRunner.js:400:11\n    at F:\\realtimepizza\\node_modules\\loader-runner\\lib\\LoaderRunner.js:252:18\n    at context.callback (F:\\realtimepizza\\node_modules\\loader-runner\\lib\\LoaderRunner.js:124:13)\n    at F:\\realtimepizza\\node_modules\\sass-loader\\dist\\index.js:54:7\n    at Function.call$2 (F:\\realtimepizza\\node_modules\\sass\\sass.dart.js:118395:16)\n    at render_closure1.call$2 (F:\\realtimepizza\\node_modules\\sass\\sass.dart.js:101951:12)\n    at _RootZone.runBinary$3$3 (F:\\realtimepizza\\node_modules\\sass\\sass.dart.js:35764:18)\n    at _FutureListener.handleError$1 (F:\\realtimepizza\\node_modules\\sass\\sass.dart.js:34280:21)\n    at _Future__propagateToListeners_handleError.call$0 (F:\\realtimepizza\\node_modules\\sass\\sass.dart.js:34595:49)\n    at Object._Future__propagateToListeners (F:\\realtimepizza\\node_modules\\sass\\sass.dart.js:5086:77)\n    at _Future._completeError$2 (F:\\realtimepizza\\node_modules\\sass\\sass.dart.js:34446:9)\n    at _AsyncAwaitCompleter.completeError$2 (F:\\realtimepizza\\node_modules\\sass\\sass.dart.js:34062:12)\n    at Object._asyncRethrow (F:\\realtimepizza\\node_modules\\sass\\sass.dart.js:4874:17)\n    at F:\\realtimepizza\\node_modules\\sass\\sass.dart.js:25447:20\n    at _wrapJsFunctionForAsync_closure.$protected (F:\\realtimepizza\\node_modules\\sass\\sass.dart.js:4899:15)\n    at _wrapJsFunctionForAsync_closure.call$2 (F:\\realtimepizza\\node_modules\\sass\\sass.dart.js:34081:12)\n    at _awaitOnObject_closure0.call$2 (F:\\realtimepizza\\node_modules\\sass\\sass.dart.js:34075:25)\n    at _RootZone.runBinary$3$3 (F:\\realtimepizza\\node_modules\\sass\\sass.dart.js:35764:18)\n    at _FutureListener.handleError$1 (F:\\realtimepizza\\node_modules\\sass\\sass.dart.js:34280:21)\n    at _Future__propagateToListeners_handleError.call$0 (F:\\realtimepizza\\node_modules\\sass\\sass.dart.js:34595:49)\n    at Object._Future__propagateToListeners (F:\\realtimepizza\\node_modules\\sass\\sass.dart.js:5086:77)\n    at _Future._completeError$2 (F:\\realtimepizza\\node_modules\\sass\\sass.dart.js:34446:9)\n    at _AsyncAwaitCompleter.completeError$2 (F:\\realtimepizza\\node_modules\\sass\\sass.dart.js:34062:12)\n    at Object._asyncRethrow (F:\\realtimepizza\\node_modules\\sass\\sass.dart.js:4874:17)\n    at F:\\realtimepizza\\node_modules\\sass\\sass.dart.js:19005:20\n    at _wrapJsFunctionForAsync_closure.$protected (F:\\realtimepizza\\node_modules\\sass\\sass.dart.js:4899:15)\n    at _wrapJsFunctionForAsync_closure.call$2 (F:\\realtimepizza\\node_modules\\sass\\sass.dart.js:34081:12)\n    at _awaitOnObject_closure0.call$2 (F:\\realtimepizza\\node_modules\\sass\\sass.dart.js:34075:25)\n    at _RootZone.runBinary$3$3 (F:\\realtimepizza\\node_modules\\sass\\sass.dart.js:35764:18)\n    at _FutureListener.handleError$1 (F:\\realtimepizza\\node_modules\\sass\\sass.dart.js:34280:21)\n    at _Future__propagateToListeners_handleError.call$0 (F:\\realtimepizza\\node_modules\\sass\\sass.dart.js:34595:49)\n    at Object._Future__propagateToListeners (F:\\realtimepizza\\node_modules\\sass\\sass.dart.js:5086:77)\n    at _Future._completeError$2 (F:\\realtimepizza\\node_modules\\sass\\sass.dart.js:34446:9)\n    at _AsyncAwaitCompleter.completeError$2 (F:\\realtimepizza\\node_modules\\sass\\sass.dart.js:34062:12)\n    at Object._asyncRethrow (F:\\realtimepizza\\node_modules\\sass\\sass.dart.js:4874:17)\n    at F:\\realtimepizza\\node_modules\\sass\\sass.dart.js:19040:20\n    at _wrapJsFunctionForAsync_closure.$protected (F:\\realtimepizza\\node_modules\\sass\\sass.dart.js:4899:15)\n    at _wrapJsFunctionForAsync_closure.call$2 (F:\\realtimepizza\\node_modules\\sass\\sass.dart.js:34081:12)\n    at _awaitOnObject_closure0.call$2 (F:\\realtimepizza\\node_modules\\sass\\sass.dart.js:34075:25)\n    at _RootZone.runBinary$3$3 (F:\\realtimepizza\\node_modules\\sass\\sass.dart.js:35764:18)\n    at _FutureListener.handleError$1 (F:\\realtimepizza\\node_modules\\sass\\sass.dart.js:34280:21)\n    at _Future__propagateToListeners_handleError.call$0 (F:\\realtimepizza\\node_modules\\sass\\sass.dart.js:34595:49)\n    at Object._Future__propagateToListeners (F:\\realtimepizza\\node_modules\\sass\\sass.dart.js:5086:77)\n    at _Future._completeError$2 (F:\\realtimepizza\\node_modules\\sass\\sass.dart.js:34446:9)\n    at _AsyncAwaitCompleter.completeError$2 (F:\\realtimepizza\\node_modules\\sass\\sass.dart.js:34062:12)\n    at Object._asyncRethrow (F:\\realtimepizza\\node_modules\\sass\\sass.dart.js:4874:17)\n    at F:\\realtimepizza\\node_modules\\sass\\sass.dart.js:80752:24\n    at _wrapJsFunctionForAsync_closure.$protected (F:\\realtimepizza\\node_modules\\sass\\sass.dart.js:4899:15)");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	__webpack_modules__["./resources/js/app.js"]();
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./resources/scss/app.scss"]();
+/******/ 	
+/******/ })()
+;
